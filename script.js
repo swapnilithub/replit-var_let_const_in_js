c4d65e094b739d760e476ad423995c604e6d7f03
SWAPnil <105651714+swapnilithub@users.noreply.github.com>
2024-08-09 10:13:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var a =9;
+{
+  var a = "Swapnil";
+  console.log(a)  //Swapnil will be the output
+}
+console.log(a)  //Swapnil o/p
+
+let d = 2323;
+{
+  let d = "Swapnil";
+  console.log(d)  //Swapnil will be the output  
+}
+console.log(d)  //2323 will be the output
+
+const e = 2323;
+{
+  const e = "Swapnil";
+  console.log(e)  //Swapnil will be the output  
+}
+console.log(e)  //2323 will be the output
+
+const author ="Swapnil";
+// author = "34"; if we try to modify the value of author it will throw an error:
+console.log(author)
+
+d = "SWAAAAAAAAAAAAAAA";
+console.log(d) //swaaaaaa willbe the output
